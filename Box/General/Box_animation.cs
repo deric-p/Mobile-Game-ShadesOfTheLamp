3f9f308eb9f671340fcaccc2780a7e80ec9f0e00
deric-p <55450221+deric-p@users.noreply.github.com>
2019-09-17 10:59:00 -0400

    Add files via upload

---
+++ Box/General/Box_animation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+
+public class Box_animation : MonoBehaviour {
+
+	private Animator box;
+	bool active = true;
+
+
+	// Use this for initialization
+	void Start () {
+		box = GetComponent<Animator> ();
+	
+	}
+	void Awake(){
+
+			StartCoroutine ("WaitOneSecond");
+	
+
+	}
+
+	IEnumerator WaitOneSecond(){
+
+		while (active) {
+			yield return new WaitForSeconds (1);
+			box.SetInteger ("box", Random.Range (0, 5));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Box/General/Box_position.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box_position : MonoBehaviour {
+
+	float box_float=-0.5f;
+	float cameralength  = 0.3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 box_position = transform.localPosition;
+		Vector3 box_scale = transform.localScale;
+		box_scale.x = (cameralength) * (Camera.main.aspect);
+		box_scale.y = (cameralength) * (Camera.main.aspect);
+		box_position.x = (box_float)*(Camera.main.aspect);
+		transform.localPosition = box_position;
+		transform.localScale = box_scale;
+	}
+}
